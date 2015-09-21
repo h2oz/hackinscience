857114f8d728c926a8cc9102273c56c31ceaed71
h2oz <h2o@cri-paris.org>
2015-09-21 19:33:28 +0200

    mt

--- exercises/014/solution.py
+++ exercises/014/solution.py
@@ -5,7 +5,11 @@ Created on Mon Sep 21 11:33:36 2015
 @author: A
 """
 import sys
-ee = sys.argv[0]
-if ee:
-    print(ee)
-print("usage: python3 solution.py PARAM")
+ee = sys.argv
+# if ee:
+print(ee)
+print(len(ee))
+if len(ee) == 2:
+    print(sys.argv[1])
+else:
+    print("usage: python3 solution.py PARAM")
