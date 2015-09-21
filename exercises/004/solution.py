f4ea1c42aa9f73965f1d7dfa1808a605fc9faec1
h2oz <h2o@cri-paris.org>
2015-09-21 14:46:18 +0200

    mt

--- exercises/004/solution.py
+++ exercises/004/solution.py
@@ -5,8 +5,8 @@ Created on Mon Sep 21 11:33:36 2015
 @author: A
 """
 alphabet = 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',\
- 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-vowel = 'a', 'e', 'i', 'o', 'u'
+           'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+vowel = 'a', 'e', 'i', 'o', 'u', 'y'
 rev = list(reversed(alphabet))
 for x in range(len(rev)):
     if rev[x] in vowel:
