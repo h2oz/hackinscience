0d37228b5636c8833663eabc654cb72c86e11882
h2oz <h2o@cri-paris.org>
2015-09-21 22:29:15 +0200

    mt

---
+++ exercises/015/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:33:36 2015
+
+@author: A
+"""
+import operator
+# r = 245850922 / 78256779
+rr = operator.truediv(245850922, 78256779)
+tt = (1+operator.pow(5, 0.5))/2
+# t= (1+5**0.5)/2
+print(rr, '\n', tt)
