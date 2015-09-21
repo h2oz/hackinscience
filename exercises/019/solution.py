0254e33ad00bfbe2c7360572492bf3faa84c7456
h2oz <h2o@cri-paris.org>
2015-09-22 01:03:28 +0200

    mt

---
+++ exercises/019/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:33:36 2015
+
+@author: A
+"""
+import sys
+# import numbers
+ee = sys.argv
+if len(ee) == 3:
+    print(int(ee[1])+int(ee[2]))
+else:
+    print("usage: python3 solution.py OP1 OP2")
