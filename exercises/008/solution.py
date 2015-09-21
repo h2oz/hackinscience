c31472f4ecd40ad070fdc2530fa198dfaffb96c3
h2oz <h2o@cri-paris.org>
2015-09-21 15:42:18 +0200

    mt

---
+++ exercises/008/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:33:36 2015
+
+@author: A
+"""
+a = "Hello World !"
+#for x in range(len(a)):
+#    print(a[x] + '\t')
+for y in a:
+    print(y)
