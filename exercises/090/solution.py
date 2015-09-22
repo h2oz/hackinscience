a9bdfbd0c5307825f86dfa4213b389d885c7ee88
h2oz <h2o@cri-paris.org>
2015-09-22 20:02:07 +0200

    mt

--- exercises/090/solution.py
+++ exercises/090/solution.py
@@ -8,5 +8,5 @@ Created on Tue Sep 22 19:12:30 2015
 import sys
 liste = sys.argv
 for x in range(len(liste)):
-    r=list(enumerate(liste))[x]
-    print(r[0],r[1])
+    r = list(enumerate(liste))[x]
+    print(r[0], r[1])
