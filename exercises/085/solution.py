24756f13f2f019ca4d73755d643c7c2450cd3b9c
h2oz <h2o@cri-paris.org>
2015-09-22 19:10:28 +0200

    mt

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -13,13 +13,13 @@ def sort_a_list(liste):
 
 
 def sort_by_mark(liste2):
-#   zz = list(map(operator.itemgetter(0),liste))
-#   liste2 = sorted(liste2, key=lambda liste2: liste2[0], reverse=True)
+    # zz = list(map(operator.itemgetter(0),liste))
+    # liste2 = sorted(liste2, key=lambda liste2: liste2[0], reverse=True)
     liste2.sort(key=lambda liste2: liste2[0], reverse=True)
     return liste2
 
 
 def sort_by_name(liste2):
-#   sorted(liste2, key=lambda liste2: liste2[1])
+    # sorted(liste2, key=lambda liste2: liste2[1])
     liste2.sort(key=lambda liste2: liste2[1])
     return liste2
