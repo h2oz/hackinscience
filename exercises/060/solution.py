999d7d060a6576c1394ed69a58bd7857e2a306e3
h2oz <h2o@cri-paris.org>
2015-09-22 13:53:16 +0200

    mt

---
+++ exercises/060/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:33:36 2015
+
+@author: A
+"""
+al = 'abcdefghijklmnopqrstuvwxyz'
+ll = len(al)
+for ii in range(ll):
+    for jj in range(ii, ll):
+        print(al[ii]+al[jj])
