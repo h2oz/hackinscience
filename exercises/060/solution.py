f6a3bbdbb529242ef9113e2ccf3b7c5a9809095f
h2oz <h2o@cri-paris.org>
2015-09-22 13:54:44 +0200

    mt

--- exercises/060/solution.py
+++ exercises/060/solution.py
@@ -7,5 +7,5 @@ Created on Mon Sep 21 11:33:36 2015
 al = 'abcdefghijklmnopqrstuvwxyz'
 ll = len(al)
 for ii in range(ll):
-    for jj in range(ii, ll):
+    for jj in range(ll):
         print(al[ii]+al[jj])
