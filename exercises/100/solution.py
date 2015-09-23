68d3495614156eb38d7ce2b1365663d609ed0db9
h2oz <h2o@cri-paris.org>
2015-09-23 16:41:14 +0200

    mt

--- exercises/100/solution.py
+++ exercises/100/solution.py
@@ -6,10 +6,10 @@ Created on Wed Sep 23 16:28:08 2015
 """
 station = {
  'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
-  'number': 31705,
-   'latitude': 48.8645278209514,
-   'name': 'CHAMPEAUX (BAGNOLET)',
-    'longitude': 2.416170724425901
+ 'number': 31705,
+ 'latitude': 48.8645278209514,
+ 'name': 'CHAMPEAUX (BAGNOLET)',
+ 'longitude': 2.416170724425901
 }
 
 print("latitide", station['latitude'])
