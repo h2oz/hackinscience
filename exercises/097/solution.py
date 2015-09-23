f3ba081f6a2a5af82145eec20d7a30e297902556
h2oz <h2o@cri-paris.org>
2015-09-23 17:09:01 +0200

    mt

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -10,10 +10,11 @@ def love_meet(liste1, liste2):
     listecommune = list(set(liste2).intersection(liste1))
     listecommune.sort(key=lambda listecommune: listecommune[0])
     print(set(listecommune))
+    return
 
 
 def affair_meet(liste1, liste2, liste3):
     listep = list(set(liste1) - set(liste2))
     listecommune = list(set(listep).intersection(liste3))
-    listecommune
     print(set(listecommune))
+    return
