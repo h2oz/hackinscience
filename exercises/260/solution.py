32ede7f0679b0e37982b2257683014505f3d782e
h2oz <h2o@cri-paris.org>
2015-09-24 18:37:05 +0200

    mt

--- exercises/260/solution.py
+++ exercises/260/solution.py
@@ -9,20 +9,26 @@ import math
 
 
 def euclidean(a, b):
-    dist = ((b[0]-a[0])**2+(b[1]-a[1])**2)**0.5
+    zz = 0
+    for x in range(len(a)):
+        zz = zz + (a[x]-b[x])**2
+    dist = (zz)**0.5
     return(dist)
 
 
 def opt_euclidean(a, b):
-    dist = math.sqrt(math.pow((b[0]-a[0]), 2) + math.pow((b[1]-a[1]), 2))
+    zz = 0
+    for x in range(len(a)):
+        zz = zz + math.pow((a[x]-b[x]), 2)
+    dist = math.sqrt(zz)
     return(dist)
 
 
 def np_euclidean(a, b):
-    pt_1 = np.array((a[0], a[1]))
-    pt_2 = np.array((b[0], b[1]))
-    dist = np.linalg.norm(pt_1-pt_2)
-    # dist = np.sqrt(np.power((b[0]-a[0]), 2) + np.power((b[1]-a[1]), 2))
+    # pt_1 = np.array((a[0], a[1]))
+    # pt_2 = np.array((b[0], b[1]))
+    # dist = np.linalg.norm(pt_1-pt_2)
+    dist = np.sqrt(np.power((b[0]-a[0]), 2) + np.power((b[1]-a[1]), 2))
     return(dist)
 
 a = [2, 3]
@@ -30,3 +36,4 @@ b = [5, 6]
 print(euclidean(a, b))
 print(opt_euclidean(a, b))
 print(np_euclidean(a, b))
+print(euclidean(a, b) == opt_euclidean(a, b) == np_euclidean(a, b))
