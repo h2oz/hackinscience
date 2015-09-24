b9e1663e20541a01771e75cb70c4fb1b6a55015d
h2oz <h2o@cri-paris.org>
2015-09-24 16:22:11 +0200

    mt

---
+++ exercises/250/solution.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 15:38:36 2015
+
+@author: A
+"""
+
+
+def draw_n_squares(n):
+    for k in range(n):
+        for i in range(n):
+            print('+---', end=""),
+        print('+')
+        for j in range(n):
+            print('|   ', end=""),
+        print('|')
+    for i in range(n):
+        print('+---', end=""),
+    print('+')
+    return("")
+
+print(draw_n_squares(2))
