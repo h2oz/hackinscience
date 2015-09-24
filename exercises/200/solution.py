9d87fdee48575a8dc6add013668aa2be4d5777e7
h2oz <h2o@cri-paris.org>
2015-09-24 15:02:21 +0200

    mt

--- exercises/200/solution.py
+++ exercises/200/solution.py
@@ -7,13 +7,15 @@ Created on Thu Sep 24 14:39:26 2015
 
 
 def is_prime(num):
-    for i in range(1, num):
+    if(num == 0 or num == 1):
+        return False
+    for i in range(2, num):
         if(i != num):
             if(num % i == 0):
                 # print(num, i, num % i)
                 return False
     return True
 
-# print(is_prime(15))
-# print(is_prime(11))
-# print(is_prime(113))
+#    print(is_prime(0))
+#    print(is_prime(1))
+#    print(is_prime(113))
