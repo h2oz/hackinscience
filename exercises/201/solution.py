47c86d1c78e6a1a32c23acc65a2129b67a2ece7a
h2oz <h2o@cri-paris.org>
2015-09-24 15:14:48 +0200

    mt

---
+++ exercises/201/solution.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 14:39:26 2015
+
+@author: A
+"""
+
+
+def is_alpha(st):
+    if(st.isalpha()):
+        return True
+    return False
+
+# print(is_alpha('nonce'))
+#    print(is_prime(1))
+#    print(is_prime(113))
