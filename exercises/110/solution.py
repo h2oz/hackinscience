c7ef51c075efc1bdfb08fc01567009b2ea66b2b9
h2oz <h2o@cri-paris.org>
2015-09-24 13:50:08 +0200

    mt

--- exercises/110/solution.py
+++ exercises/110/solution.py
@@ -16,16 +16,24 @@ def is_int(s):
     except ValueError:
         return False
 
+
+def is_ops(s):
+    if((s == "+") or (s == "-") or (s == "\*") or (s == "//") or
+       (s == "%") or (s == "^")):
+        return True
+    else:
+        return False
+
 zz = sys.argv
 ops = ({"+": operator.add, "-": operator.sub, "\*": operator.mul,
        "//": operator.truediv, "%": operator.mod, "^": operator.pow})
 if(len(zz) == 4):
     if(is_int(zz[1]) and (type(zz[2]) is str) and
-       is_int(zz[3])):
+       is_int(zz[3]) and is_ops(zz[2])):
         print(ops[zz[2]](int(zz[1]), int(zz[3])))
     else:
-        print("input error")
+        print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
 else:
-    print("input error")
+    print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
 #   for i in range(len(zz)):
 #   print(type(zz[i]))
