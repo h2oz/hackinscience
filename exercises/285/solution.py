487c568cf49281b1e8a4f023519a7f75064ea705
h2oz <h2o@cri-paris.org>
2015-09-25 22:39:12 +0200

    mt

---
+++ exercises/285/solution.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 21:54:10 2015
+
+@author: A
+"""
+import requests
+r = requests.get('http://mdk.fr/ip')
+try:
+    # r.status_code
+    # r.headers['content-type']
+    # r.encoding
+    print(r.text)
+except:
+    print('Not enough parameters.')
