2a77cc76568b88911ff7419b5961c53584d58e8a
h2oz <h2o@cri-paris.org>
2015-09-27 01:43:49 +0200

    mt

---
+++ exercises/330/solution.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 27 00:31:49 2015
+
+@author: A
+"""
+import json
+# from pprint import pprint
+
+
+def load_jason(path):
+    f = open(path, 'r')
+    # d = f.readlines()
+    f.closed
+    d = json.load(f)
+    # d = dict(zip(d[0::2], d[1::2]))
+    # pprint(d)
+    return(d)
+
+load_jason("example.json")
