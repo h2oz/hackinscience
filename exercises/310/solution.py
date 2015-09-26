332698a165d246618b3a8848900a3e93086014db
h2oz <h2o@cri-paris.org>
2015-09-26 13:37:21 +0200

    mt

--- exercises/310/solution.py
+++ exercises/310/solution.py
@@ -6,11 +6,13 @@ Created on Sat Sep 26 11:20:32 2015
 """
 # with open("words") as myfile:
 f = open("words", 'r')
-# t = f.readlines()
-# u = t.count("e")
+t = f.readlines()
+j = len(t)
 u = 0
-for line in f:
-    if "e" in line:
-        u += 1
+for line in range(0, j):
+    # print(t[line], "", line)
+    if "e" in t[line]:
+        u = u + t[line].count("e")
+        # print(line, "" , u, end="")
 print(u)
 f.closed
