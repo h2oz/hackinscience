b33feb451a241d6e3bff4df446503ef66d8830b0
h2oz <h2o@cri-paris.org>
2015-09-26 21:06:19 +0200

    mt

--- exercises/320/solution.py
+++ exercises/320/solution.py
@@ -10,14 +10,23 @@ t = f.readlines()
 j = len(t)
 al = 'abcdefghijklmnopqrstuvwxyz'
 alp = dict((k, 1) for k in al)
+sline = 0
 for x in range(len(alp)):
     lettre = list(alp)[x]
     u = 0
     for line in range(0, j):
         # print(t[line], "", line)
+        # sline = sline + len(t[line])
         if lettre in t[line]:
             u = u + t[line].count(lettre)
     alp[lettre] = u
     # print(lettre, "", u, end="")
-print(alp)
+# somme = sum(alp.values())
+for line in range(0, j):
+    sline = sline + len(t[line])
+print(somme, sline)
+for x in range(len(alp)):
+    lettre = list(alp)[x]
+    alp[lettre] = alp[lettre]/sline
+    print(lettre+":", "%.2f" % alp[lettre])
 f.closed
