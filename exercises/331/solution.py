cd8649bd2f17a04c4fd35fc593b2bb2acc93f82c
h2oz <h2o@cri-paris.org>
2015-10-01 00:31:04 +0200

    mt

---
+++ exercises/331/solution.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 27 00:31:49 2015
+
+@author: A
+"""
+import json
+# from pprint import pprint
+
+
+def load_json(path):
+    f = open(path, 'r')
+    # d = f.readlines()
+    d = json.load(f)
+    f.closed
+    # d = dict(zip(d[0::2], d[1::2]))
+    # pprint(d)
+    return(d)
+
+
+def test_int(inp):
+    try:
+        return(int(inp))
+    except:
+        return(False)
+
+
+d = load_json("velib.json")
+# print(len(d))
+for i in range(len(d)):
+    # print(d[i]['address'])
+    l = d[i]['address'].lstrip()
+    m = d[i]['name'].lstrip()
+    # print(d[i])
+    for j in range(len(l), -1, -1):
+        if(test_int(l[j-1])):
+            # print(l[j-1:j+4])
+            # print(l[j+(5):len(l)])
+            d[i]['zip_code'] = l[j-5:j+1].lstrip()
+            d[i]['city'] = l[j+1:len(l)]
+            d[i]['address'] = (l[0:j-7])
+            break
+    for k in range(len(m)):
+        if(test_int(m[k])):
+            # print(l[j-1:j+4])
+            # print(l[j+(5):len(l)])
+            d[i]['name'] = m[k+7:len(m)].lstrip()
+            # print(m)
+            break
+        # break
+    # d[i].pop('name')
+    # print(d[i])
+g = open("solution.json", 'w')
+json.dump(d, g)
+g.close()
+# print(((d[i]['address'].split('- ', 1))[1]).split())
