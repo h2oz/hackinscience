3517cbf55c1bb2564f20b7d593afb17b18431b14
h2oz <h2o@cri-paris.org>
2015-10-15 14:02:57 +0200

    Update solution.py

--- exercises/260/solution.py
+++ exercises/260/solution.py
@@ -7,36 +7,17 @@ Created on Thu Sep 24 15:38:36 2015
 import numpy as np
 import math
 
-
 def euclidean(a, b):
-    zz = 0
-    for x in range(len(a)):
-        zz = zz + (a[x]-b[x])**2
-    dist = (zz)**0.5
-    return(dist)
+    return ((b[0]-a[0]) ** 2 + (b[1]-a[1]) ** 2) ** 0.5
 
 
 def opt_euclidean(a, b):
-    zz = 0
-    for x in range(len(a)):
-        zz = zz + math.pow((a[x]-b[x]), 2)
-    dist = math.sqrt(zz)
-    return(dist)
+    x = b[0] - a[0]
+    y = b[1] - a[1]
+    return math.hypot(x, y)
 
 
 def np_euclidean(a, b):
-    # pt_1 = np.array((a[0], a[1]))
-    # pt_2 = np.array((b[0], b[1]))
-    # dist = np.linalg.norm(pt_1-pt_2)
-    zz = 0
-    for x in range(len(a)):
-        zz = zz + np.power((a[x]-b[x]), 2)
-    dist = np.sqrt(zz)
-    return(float(dist))
-
-#    a = [2, 3]
-#    b = [5, 6]
-#    print(euclidean(a, b))
-#    print(opt_euclidean(a, b))
-#    print(np_euclidean(a, b))
-#    print(euclidean(a, b) == opt_euclidean(a, b) == np_euclidean(a, b))
+    x = np.array(a[0], a[1])
+    y = np.array(b[0], b[1])
+    return np.linalg.norm(x - y)
