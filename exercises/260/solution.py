667fbbddd889255ca2c66f10cb1e571429a0aa22
h2oz <h2o@cri-paris.org>
2015-10-15 15:45:15 +0200

    go

--- exercises/260/solution.py
+++ exercises/260/solution.py
@@ -34,9 +34,18 @@ def np_euclidean(a, b):
     dist = np.sqrt(zz)
     return(float(dist))
 
+
+#    def np_euclidean2(a, b):
+#        x = np.array(a[0], a[1])
+#        y = np.array(b[0], b[1])
+#        return np.linalg.norm(x - y)
+
+#    a = [2, 3, 7, 3, 4, 0, 8, 2, 1]
+#    b = [5, 3, 0, 7, 8, 6, 9, 4, 5]
 #    a = [2, 3]
 #    b = [5, 6]
 #    print(euclidean(a, b))
 #    print(opt_euclidean(a, b))
 #    print(np_euclidean(a, b))
+#    print(np_euclidean2(a, b))
 #    print(euclidean(a, b) == opt_euclidean(a, b) == np_euclidean(a, b))
