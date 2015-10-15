eb3a24d7c84b5d336e9f603fd6ade430bcad9c44
h2oz <h2o@cri-paris.org>
2015-10-15 13:57:41 +0200

    go

--- exercises/250/solution.py
+++ exercises/250/solution.py
@@ -6,17 +6,28 @@ Created on Thu Sep 24 15:38:36 2015
 """
 
 
+# def draw_n_squares(n):
+#    for k in range(n):
+#        for i in range(n):
+#            print('+---', end=""),
+#        print('+')
+#        for j in range(n):
+#            print('|   ', end=""),
+#        print('|')
+#    for i in range(n):
+#        print('+---', end=""),
+#    print('+')
+#    return ''
+#
+# print(draw_n_squares(3))
+
+
 def draw_n_squares(n):
-    for k in range(n):
-        for i in range(n):
-            print('+---', end=""),
-        print('+')
-        for j in range(n):
-            print('|   ', end=""),
-        print('|')
-    for i in range(n):
-        print('+---', end=""),
-    print('+')
-    return ''
+    draw = []
+    for i in range(0, n):
+        draw.append('+---'*n + '+')
+        draw.append('|   '*n + '|')
+    draw.append('+---'*n + '+')
+    return '\n'.join(str(item) for item in draw)
 
 # print(draw_n_squares(3))
