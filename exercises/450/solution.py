cf4e9785b4f2f4ddd0179c63a2d9e61b5a62b126
h2oz <h2o@cri-paris.org>
2015-10-15 21:17:50 +0200

    go

--- exercises/450/solution.py
+++ exercises/450/solution.py
@@ -11,22 +11,37 @@ al = list(string.ascii_lowercase)
 AL = list(string.ascii_uppercase)
 
 
-def caesar_cypher(s, key, method):
-    sol = ''
+def forward(method):
     if method == 'forward':
         si = 1
+    else:
+        si = 1
+    return(si)
+
+
+def backward(method):
     if method == 'backward':
         si = -1
+    else:
+        si = 1
+    return(si)
+
+
+def caesar_cypher(s, key, method):
+    sol = ''
+    si = forward(method) * backward(method)
     for j in range(len(s)):
         x = s[j]
+        if (x not in al) and (x not in AL):
+            sol = sol + x
         for i in range(0, len(al)):
             if x == al[i]:
                 sol = sol + (al[(i + (si * key)) % len(al)])
         for i in range(0, len(al)):
             if x == AL[i]:
-                sol = sol + (al[(i + (si * key)) % len(al)])
+                sol = sol + (AL[(i + (si * key)) % len(al)])
     print(sol)
     return()
 
 if __name__ == '__main__':
-    caesar_cypher('bol', 1, 'backward')
+    caesar_cypher('Python is super disco !', 31, 'backward')
