0bad3f54d490318390a912814ff6223c46e152b7
h2oz <h2o@cri-paris.org>
2015-10-15 16:54:58 +0200

    go

--- exercises/334/solution.py
+++ exercises/334/solution.py
@@ -5,3 +5,37 @@ Created on Thu Oct 15 15:53:45 2015
 @author: A
 """
 
+import json
+import math
+
+
+def load_json(path):
+    f = open(path, 'r', encoding="utf-8")
+    # d = f.readlines()
+    d = json.load(f)
+    f.close()
+    # d = dict(zip(d[0::2], d[1::2]))
+    # pprint(d)
+    return(d)
+
+
+def opt_euclidean(a, b):
+    x = b[0] - a[0]
+    y = b[1] - a[1]
+    return math.hypot(x, y)
+
+
+def locate(lat, long):
+    k = 0
+    d = load_json("solution.json")
+    a = [lat, long]
+    for i in range(0, len(d)):
+        b_old = [d[k]['latitude'], d[k]['longitude']]
+        b_new = [d[i]['latitude'], d[i]['longitude']]
+        if (opt_euclidean(a, b_new) <= opt_euclidean(a, b_old)):
+            k = i
+    print(d[k])
+    return(k)
+
+
+locate(48.83, 2.41)
